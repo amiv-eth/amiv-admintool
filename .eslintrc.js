5c684ef0688c7551f886829636fd97a854b3df9d
Hermann Blum <hermannsblum@yahoo.de>
2017-11-30 14:15:04 +0100

    add webpack and node structure

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "plugins": [
+        "import"
+    ],
+    "env": {
+      "browser": true,
+    },
+    "rules": {
+        "no-console": 0,
+        "class-methods-use-this": 0,
+        "prefer-destructuring": 1,
+        "no-underscore-dangle": 0,
+    }
+};
