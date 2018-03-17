db9ba59db6632aeb1a50c6297106bb22e0458577
Lionel Trébuchon <ltrebuchon@gmail.com>
2018-03-17 14:33:13 +0100

    [export] - Add button, reduce input size for lisibility.

--- src/events/eventWithExport.js
+++ src/events/eventWithExport.js
@@ -9,105 +9,34 @@ export default class eventWithExport extends EditView {
     }
 
     view() {
-        // German and English Information
+        // Editable by event creator.
         const fieldTitleEn = m(TextField, {
             label: 'Event Title [EN]',
             required: true,
             floatingLabel: true,
             dense: true,
         });
-
-        const fieldCatchphraseEn = m(TextField, {
-            label: 'Catchphrase [EN]',
-            floatingLabel: true,
-            dense: true,
-            help: 'Fun description to make your event look more interesting than it is',
-            focusHelp: true,
+        const fieldDescriptionEn = m(TextField, { 
+            label: 'Description [EN]', 
+            required: true, 
+            floatingLabel: true, 
+            dense: true, 
+            multiLine: true, 
+            rows: 6, 
         });
-
-        const fieldDescriptionEn = m(TextField, {
-            label: 'Description [EN]',
-            required: true,
-            floatingLabel: true,
-            dense: true,
-            multiLine: true,
-            rows: 6,
+        const creatorButton = m(Button, {
+            label: 'EXPORT'
+            // label: 'Create a hyperlink string describing your project.'
         });
-
-        const fieldTitleDe = m(TextField, {
-            label: 'Event Title [DE]',
-            floatingLabel: true,
-            dense: true,
-        });
-
-        const fieldCatchphraseDe = m(TextField, {
-            label: 'Catchphrase [DE]',
-            floatingLabel: true,
-            dense: true,
-            help: 'Fun description to make your event look more interesting than it is',
-            focusHelp: true,
-        });
-
-        const fieldDescriptionDe = m(TextField, {
-            label: 'Description [DE]',
-            floatingLabel: true,
-            dense: true,
-            multiLine: true,
-            rows: 6,
-        });
-        // Start of relevant data
-        const fieldPrice = m(TextField, {
-            label: 'Price:',
-            type: 'number',
-            help: 'In Rappen/Cents',
-            focusHelp: true,
-            floatingLabel: true,
-            required: true,
-        });
-        const fieldStartDate = m(TextField, {
-            label: 'Event Start[Date and Time]:',
-            help: 'Format: 01.01.1970-18:00',
-            focusHelp: true,
-            floatingLabel: true,
-            required: true,
-        });
-        const fieldEndDate = m(TextField, {
-            label: 'Event End[Date and Time]:',
-            help: 'Format: 01.01.1970-1800',
-            focusHelp: true,
-            floatingLabel: true,
-            required: true,
-        });
-        const fieldStartRegDate = m(TextField, {
-            label: 'Registration Start[Date and Time]:',
-            help: 'Format: 01.01.1970-18:00',
-            focusHelp: true,
-            floatingLabel: true,
-            required: true,
-        });
-        const fieldEndRegDate = m(TextField, {
-            label: 'Registration End[Date and Time]:',
-            help: 'Format: 01.01.1970-1800',
-            focusHelp: true,
-            floatingLabel: true,
-            required: true,
-        });
+        // Needs administrator (Kulturi).
         const fieldLocation = m(TextField, {
             label: 'Location:',
             floatingLabel: true,
             required: true,
         });
-        const fieldNumberOfParticipants = m(TextField, {
-            label: 'Number of open spots:',
-            type: 'number',
-            floatingLabel: true,
-            required: true,
-        });
 
         return m('div', [
-            //m('h1', 'Event description:', fieldTitleEn, fieldCatchphraseEn, fieldDescriptionEn,
-            //fieldTitleDe, fieldCatchphraseDe, fieldDescriptionDe),
-            m('h1', 'Critical Information:', fieldStartDate, fieldEndDate, fieldStartRegDate, fieldEndRegDate, fieldLocation, fieldPrice, fieldNumberOfParticipants),
+            m('h1', 'For the event creator:', fieldTitleEn, fieldDescriptionEn, 'For the AMIV administrator:', fieldLocation, creatorButton),
         ]);
     }
 }
