fd63931fef5ccaacd2ff54a1288acd542eadfbc4
Hermann <blumh@student.ethz.ch>
2018-05-03 20:53:31 +0200

    show, add and remove receive_from emails

--- src/groups/viewGroup.js
+++ src/groups/viewGroup.js
@@ -1,8 +1,6 @@
 import m from 'mithril';
-import {
-  Button,
-  Card,
-} from 'polythene-mithril';
+import { Button, Card, Toolbar, ToolbarTitle, TextField, Icon } from 'polythene-mithril';
+import { icons } from '../views/elements';
 import ItemView from '../views/itemView';
 import TableView from '../views/tableView';
 import DatalistController from '../listcontroller';
@@ -83,6 +81,86 @@ class MembersTable {
   }
 }
 
+// Table for list of email adresses, both forward_to and receive
+class EmailTable {
+  constructor({ attrs: { onRemove = () => {} }}) {
+    this.addmode = false;
+    this.dirty = false;
+    this.newvalue = '';
+    this.onRemove = onRemove;
+  }
+
+  item(data) {
+    return m('div', {
+      style: {
+        margin: '10px',
+        padding: '5px',
+        height: '30px',
+        'background-color': '#dddddd',
+      },
+    }, [
+      data,
+      m(Icon, {
+        style: { 'margin-left': '3px' },
+        svg: { content: m.trust(icons.clear) },
+        size: 'small',
+        events: {
+          onclick: () => { this.onRemove(data); },
+        },
+      }),
+    ]);
+  }
+
+  view({ attrs: { list, onSubmit = () => {} } }) {
+    return m(Card, {
+      style: { height: '200px' },
+      content: m('div', [
+        this.addmode ? m(Toolbar, {
+          compact: true,
+          style: { background: 'rgb(78, 242, 167)' },
+        }, [
+          m(TextField, {
+            label: 'enter email address',
+            type: 'email',
+            onChange: ({ value }) => {
+              this.dirty = value !== '';
+              this.newvalue = value;
+            },
+          }),
+          m(Button, {
+            label: this.dirty ? 'Submit' : 'Cancel',
+            className: 'blue-button',
+            events: {
+              onclick: () => {
+                if (this.dirty) {
+                  onSubmit(this.newvalue);
+                  this.addmode = false;
+                  this.newvalue = '';
+                } else {
+                  this.addmode = false;
+                }
+              },
+            },
+            value: this.newvalue,
+          }),
+        ]) : '',
+        m(Toolbar, { compact: true }, [
+          m(ToolbarTitle, { text: 'Receiving Email Adresses' }),
+          m(Button, {
+            className: 'blue-button',
+            borders: true,
+            label: 'add',
+            events: { onclick: () => { this.addmode = true; } },
+          }),
+        ]),
+        m('div', {
+          style: { padding: '10px', display: 'flex', 'flex-wrap': 'wrap' },
+        }, list.map(item => this.item(item))),
+      ]),
+    });
+  }
+}
+
 export default class viewGroup extends ItemView {
   constructor() {
     super('groups');
@@ -98,17 +176,45 @@ export default class viewGroup extends ItemView {
   view({ attrs: { onEdit } }) {
     if (!this.data) return '';
 
-    return m('div', {
-      style: { height: '100%', 'overflow-y': 'scroll', padding: '10px' },
+    return m('div.maincontainer', {
+      style: { height: '100%', 'overflow-y': 'scroll' },
     }, [
       // this div is the title line
       m('div', [
         // event image if existing
         m('h1', { style: { 'margin-top': '0px', 'margin-bottom': '0px' } }, this.data.name),
       ]),
-      m('div', [
-        m('h4', 'Members'),
-        m(MembersTable, { group: this.id }),
+      m('div.viewcontainer', [
+        m('div.viewcontainercolumn', [
+          m('h4', 'Members'),
+          m(MembersTable, { group: this.id }),
+        ]),
+        m('div.viewcontainercolumn', [
+          m(EmailTable, {
+            list: this.data.receive_from || [],
+            onSubmit: (newItem) => {
+              const oldList = this.data.receive_from || [];
+              this.handler.patch({
+                _id: this.data._id,
+                _etag: this.data._etag,
+                receive_from: [...oldList, newItem],
+              }).then((newData) => { this.data = newData; });
+            },
+            onRemove: (item) => {
+              const oldList = this.data.receive_from;
+              // remove the first occurence of the given item-string
+              const index = oldList.indexOf(item);
+              if (index !== -1) {
+                oldList.splice(index, 1);
+                this.handler.patch({
+                  _id: this.data._id,
+                  _etag: this.data._etag,
+                  receive_from: oldList,
+                }).then((newData) => { this.data = newData; });
+              }
+            },
+          }),
+        ]),
       ]),
     ]);
   }
