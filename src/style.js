1edf9663c3bc453f0c18732ecab16fc8aa4f9a05
Hermann <blumh@ethz.ch>
2018-06-07 21:53:44 +0200

    update polythene styling to look more like the recent material design update

--- src/style.js
+++ src/style.js
@@ -1,22 +1,25 @@
-import { ButtonCSS, addTypography } from 'polythene-css';
+import { ButtonCSS, addTypography, CardCSS, ShadowCSS } from 'polythene-css';
 import { styler } from 'polythene-core-css';
 
 addTypography();
 
+// https://material.io/tools/color/#!/?view.left=0&view.right=1&secondary.color=e8462b&primary.color=274284
 export const colors = {
-	amiv_blue: '#1F2D54',
-	amiv_red: '#e8462b',
-	green: '#4ef599',
-	light_blue: '#5378E1',
-	orange: 'orange',
-}
+  amiv_blue: '#1F2D54',
+  amiv_red: '#e8462b',
+  green: '#4ef599',
+  blue: '#274284',
+  //light_blue: '#5378E1',
+  light_blue: '#5a6db4',
+  orange: 'orange',
+};
 
 ButtonCSS.addStyle('.blue-button', {
-  color_light_text: colors.light_blue,
+  color_light_text: colors.blue,
 });
 
 ButtonCSS.addStyle('.blue-button-filled', {
-  color_light_background: colors.amiv_blue,
+  color_light_background: colors.blue,
   color_light_text: 'white',
 });
 
@@ -26,6 +29,15 @@ ButtonCSS.addStyle('.red-row-button', {
   padding_h: 0,
   font_size: 12,
   margin_h: 0,
+});
+
+CardCSS.addStyle('.pe-card', {
+  border_radius: '4',
+});
+
+ShadowCSS.addStyle('.pe-shadow', {
+  'shadow-top-z-1': 'initial',
+  'shadow-bottom-z-1': '0 0 1px 0 rgba(0, 0, 0, 0.37)',
 });
 
 // style for general containers
