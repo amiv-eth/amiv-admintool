1558c3666052a5414079e60bac76a71d16cff2c8
Lionel Trébuchon <ltrebuchon@gmail.com>
2018-10-13 20:38:20 +0200

    [viewJobs] - List details nicely below the title

--- src/jobs/viewJob.js
+++ src/jobs/viewJob.js
@@ -28,6 +28,8 @@ class DuoLangProperty {
 
 export default class viewJob extends ItemView {
   view() {
+    const stdMargin = { margin: '5px' };
+
     return this.layout([
       m('div', [
         // company logo if existing
@@ -47,6 +49,21 @@ export default class viewJob extends ItemView {
           style: { 'margin-top': '0px', 'margin-bottom': '0px' },
         }, [this.data.title_de || this.data.title_en]),
       ]),
+      // below the title, most important details are listed
+      m('div', { style: { display: 'flex' } }, [
+        m(
+          Property, { title: 'Expected starting time:', style: stdMargin },
+          this.data.time_start ? `${dateFormatter(this.data.time_start)}` : 'Unknown.',
+        ),
+        m(
+          Property, { title: 'Potential duration:', style: stdMargin },
+          this.data.duration ? `${dateFormatter(this.data.duration)}` : 'Unknown.',
+        ),
+        m(
+          Property, { title: 'Offer ends:', style: stdMargin },
+          this.data.time_advertising_end ? `${dateFormatter(this.data.time_advertising_end)}` : 'If you see this message, please inform the AMIV committee that they forgot to add time_advertising_end for this offer.'
+        ),
+      ]),
       // m('div.viewcontainercolumn', [
       m('div', [
         this.data.location ? m(Property, { title: 'Location' }, `${this.data.location}`) :
@@ -61,10 +78,6 @@ export default class viewJob extends ItemView {
           en: m('p', m.trust(marked(escape(this.data.description_en)))),
         }),
       ]),
-      // below the title, most important details are listed
-      this.data.time_end ? m(Property, {
-        title: 'Offer Ends',
-      }, `${dateFormatter(this.data.time_end)}`) : '',
     ]);
   }
 }
