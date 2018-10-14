c562805d5a7b1697e8f55941b0efaacebc4aa532
Christian H <chegelba@student.ethz.ch>
2018-10-14 15:33:44 +0200

    Add multi file upload to studydocuments

--- src/studydocs/editDoc.js
+++ src/studydocs/editDoc.js
@@ -1,23 +1,25 @@
 import m from 'mithril';
-import { RadioGroup } from 'polythene-mithril';
+import { fileInput } from 'amiv-web-ui-components';
+import { RadioGroup, Button, List, ListTile } from 'polythene-mithril';
 import EditView from '../views/editView';
 
 
 export default class editDoc extends EditView {
+  // constructoe^r zu file upload
+  constructor(vnode) {
+    super(vnode);
+    if (!('files' in this.form.data)) {
+      this.form.data.files = [{ name: 'add file' }];
+    }
+  }
+
   view() {
     return this.layout([
       m('h3', 'Add a New Studydocument'),
-      ...this.form.renderPage({
-        // uploader
-        author: { type: 'text', label: 'Author' },
-        files: { type: 'text', label: 'File' }, // buggy only singel file possible
-        lecture: { type: 'text', label: 'Lecture' },
-        title: { type: 'text', label: 'Title' },
-        professor: { type: 'text', label: 'Professor' },
-        course_year: { type: 'number', lable: 'Year' }, // semester unterscheidung, plausibility
-      }),
+
       // department //drop-down-list
-      m('div', 'Semester'), // formatieren
+      // lable for RadioGroup: semester
+      m('div', { style: { color: '#0006', 'font-size': '16px' } }, 'Semester'),
       m(RadioGroup, {
         name: 'semester',
         buttons: [
@@ -27,7 +29,7 @@ export default class editDoc extends EditView {
           { value: '4', label: '4', defaultChecked: this.form.data.gender === '4' },
           { value: '5', label: '5+', defaultChecked: this.form.data.gender === '5' },
         ],
-        onChange: ({ value }) => { console.log(value); this.form.data.gender = value; },
+        onChange: ({ value }) => { console.log(value); this.form.data.semester = value; },
       }),
       m(RadioGroup, {
         name: 'type',
@@ -50,7 +52,37 @@ export default class editDoc extends EditView {
         }],
         onChange: ({ value }) => { console.log(value); this.form.data.gender = value; },
       }),
+      ...this.form.renderPage({
+        lecture: { type: 'text', label: 'Lecture' },
+        title: { type: 'text', label: 'Title' },
+        course_year: { type: 'number', label: 'Year' },
+        professor: { type: 'text', label: 'Professor' },
+        author: { type: 'text', label: 'Author' },
+      }),
+      // file upload: unfinished
+      m('div', [
+        m(List, {
+          tiles: this.form.data.files.map(file => m(ListTile, {
+            content: [
+              m(fileInput, this.form.bind({
+                name: 'new_file',
+                label: `${file.name}`,
+              })),
+            ],
+          })),
+        }),
 
+
+        // additional file
+        m(Button, {
+          label: 'Additional File',
+          className: 'blue-button',
+          border: true,
+          // onclick to be enabled
+          events: { onclick: () => { this.form.data.files.push({ name: 'add file' }); } },
+
+        }),
+      ]),
     ]);
   }
 }
