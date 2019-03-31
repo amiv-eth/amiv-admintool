c5074dacd8fa04eb4cc9b260b04d5f6c1d31c3e6
Hermann <blumh@ethz.ch>
2019-03-31 21:20:59 +0200

    overwrite onchange

--- src/groups/editGroup.js
+++ src/groups/editGroup.js
@@ -55,11 +55,11 @@ class PermissionEditor {
           options: ['no permission', 'read', 'readwrite'],
           default: 'no permission',
           style: { width: '200px' },
-          onChange: ({ newVal }) => {
-            if (newVal === 'no permission') {
+          onchange({ target: { value } }) {
+            if (value === 'no permission') {
               // the api equivalent to no permission is to delete the key out of the dict
               if (internalPerm[apiEndpoint.href]) delete internalPerm[apiEndpoint.href];
-            } else internalPerm[apiEndpoint.href] = newVal;
+            } else internalPerm[apiEndpoint.href] = value;
             onChange(internalPerm);
           },
           value: internalPerm[apiEndpoint.href],
