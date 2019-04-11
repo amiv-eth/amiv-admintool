3c7ed01805224d4461a1fe013ef675700915668d
Ian Boschung <ian@ianbo.ch>
2019-04-11 16:55:35 +0200

    Addes membership info event view
    Displays the membership info in the list with participants of an event.

--- src/events/viewEvent.js
+++ src/events/viewEvent.js
@@ -196,6 +196,7 @@ class ParticipantsTable {
         ...data.user ? [`${data.user.firstname} ${data.user.lastname}`, m('br')] : '',
         data.email,
       ]),
+      m('div', { style: { width: '8em' } }, ...data.user ? data.user.membership : ''),
       m(
         'div', { style: { width: '16em' } },
         m('div', ...data.user ? `Membership: ${data.user.membership}` : ''),
@@ -242,6 +243,7 @@ class ParticipantsTable {
           titles: [
             { text: 'Date of Signup', width: '9em' },
             { text: 'Participant', width: '16em' },
+            { text: 'Membership', width: '8em' },
             { text: 'Additional Info', width: '16em' },
           ],
         }),
